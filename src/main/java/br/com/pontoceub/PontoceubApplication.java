3b8a2654bf13448beadc2f665fc66455ab5db153
vitor costa <vitorjjhg@hotmail.com>
2024-05-06 14:19:33 -0300

    properties

--- src/main/java/br/com/pontoceub/PontoceubApplication.java
+++ src/main/java/br/com/pontoceub/PontoceubApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PontoceubApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PontoceubApplication.class, args);
 	}
